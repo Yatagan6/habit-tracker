a4c2012f9a90c6d2a1327ef71f255f9eaa21f9d6
Salavat <salavat@MacBook-Air-Salavat.local>
2026-01-11 16:51:24 +0300

    Initial commit

---
+++ habit_tracker.py
@@ -0,0 +1,26 @@
+habits = []
+def show_menu():
+    print("\nТрекер привычек")
+    print("1. Показать привычки")
+    print("2. Добавить привычку")
+    print("3. Отметить привычку выполненной")
+    print("4. Выход")
+def main():
+    while True:
+        show_menu()
+        choice = input("Выберите действие:")
+        if choice == "4":
+            print("Выход из программы")
+            break
+        elif choice == "2":
+            add_habit()
+        else:
+            print("Неверный пункт меню")
+def add_habit():
+    name = input("Введите название привычки:")
+    habits.append({
+        "name": name,
+        "done": False
+    })
+    print("Привычка добавлена")
+main()
---
+++ warmup.py
@@ -0,0 +1,4 @@
+def say_hello(name):
+    print("Привет,", name)
+say_hello("Салават")
+say_hello("Мир")
